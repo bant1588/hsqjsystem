14dedbc75501e39ed01286c27900679c663d9aae
bant1588 <2719019290@qq.com>
2026-04-16 09:06:22 +0800

    Add files via upload

--- components/TaxTableRenderer.js
+++ components/TaxTableRenderer.js
@@ -1,48 +1,63 @@
 // components/TaxTableRenderer.js
+import { ref } from 'https://unpkg.com/vue@3/dist/vue.esm-browser.js'
 import { db } from '../js/engine.js'
 
 export default {
-    props: { config: { type: Object, required: true } },
+    props: ['config'],
+    setup() { return { db } },
     template: `
-        <div class="form-card">
-            <h2 style="text-align:center;margin-bottom:20px;">{{ config.title }}</h2>
-            <table class="tax-table">
-                <thead>
-                    <tr>
-                        <th v-for="(col, i) in config.columns" :key="i" :width="col.width" :style="{textAlign: col.align || 'center'}">
-                            {{ col.title }}
-                        </th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <tr v-for="(row, idx) in config.rows" :key="idx">
-                        <td style="text-align:center;">{{ row.line }}</td>
-                        <td :style="{ textAlign:'left', paddingLeft: (row.indent || 0)*20 + 10 + 'px', fontWeight: row.isBold?'bold':'normal' }">
-                            {{ row.text }}
-                        </td>
-                        
-                        <template v-if="row.inputs">
-                            <td v-for="(inp, iIdx) in row.inputs" :key="iIdx" :style="{ background: inp.isAsterisk?'#f5f5f5':'white', textAlign: 'center' }">
-                                <span v-if="inp.isAsterisk" style="color:#999; font-weight:bold;">*</span>
-                                <input v-else-if="inp.key && inp.type === 'text'" type="text" class="tax-input text-center" 
-                                       v-model="db[config.id][inp.key]" :readonly="inp.isReadonly" :placeholder="inp.placeholder || ''">
-                                <input v-else-if="inp.key" type="number" class="tax-input" 
-                                       v-model.number="db[config.id][inp.key]" :readonly="inp.isReadonly">
-                            </td>
-                        </template>
-                        
-                        <template v-else>
-                            <td style="text-align: center;">
-                                <input v-if="row.key && row.type === 'text'" type="text" class="tax-input text-center" 
-                                       v-model="db[config.id][row.key]" :readonly="row.isReadonly">
-                                <input v-else-if="row.key" type="number" class="tax-input" 
-                                       v-model.number="db[config.id][row.key]" :readonly="row.isReadonly">
-                            </td>
-                        </template>
-                    </tr>
-                </tbody>
-            </table>
-        </div>
-    `,
-    setup() { return { db } }
+    <table class="tax-table">
+        <thead v-if="config.headers">
+            <tr v-for="(hRow, rIdx) in config.headers" :key="'h'+rIdx">
+                <th v-for="(hCol, cIdx) in hRow" :key="'hc'+cIdx" 
+                    :colspan="hCol.colspan || 1" :rowspan="hCol.rowspan || 1" 
+                    :style="{width: hCol.width}">
+                    <div v-html="hCol.title.replace(/\\n/g, '<br>')"></div>
+                </th>
+            </tr>
+        </thead>
+        <thead v-else>
+            <tr>
+                <th v-for="(col, idx) in config.columns" :key="idx" :width="col.width">
+                    <div v-html="col.title.replace(/\\n/g, '<br>')"></div>
+                </th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr v-for="(row, rIdx) in config.rows" :key="rIdx">
+                <td v-if="row.line" :rowspan="row.lineRowspan || 1" :colspan="row.lineColspan || 1" class="text-center">{{ row.line }}</td>
+                <td v-if="row.text" :rowspan="row.textRowspan || 1" :colspan="row.textColspan || 1" class="text-left" 
+                    :style="{ paddingLeft: row.indent ? (row.indent * 20 + 12 + 'px') : '12px', fontWeight: row.isBold ? 'bold' : 'normal' }">
+                    {{ row.text }}
+                </td>
+                <td v-if="row.text2" :rowspan="row.text2Rowspan || 1" :colspan="row.text2Colspan || 1" 
+                    :class="row.align2 === 'left' ? 'text-left' : 'text-center'" :style="{ fontWeight: row.isBold2 ? 'bold' : 'normal' }">
+                    {{ row.text2 }}
+                </td>
+
+                <td v-for="(input, iIdx) in row.inputs" :key="iIdx" 
+                    :colspan="input.colspan || 1" :rowspan="input.rowspan || 1"
+                    :style="input.style">
+                    
+                    <div v-if="input.type === 'label'" v-html="input.value" :style="input.innerStyle"></div>
+                    
+                    <span v-else-if="input.isAsterisk">*</span>
+                    
+                    <div v-else-if="input.type === 'radio'" class="radio-group" style="justify-content: center;">
+                        <label v-for="opt in input.options" :key="opt">
+                            <input type="radio" :name="config.id + '_' + input.key" :value="opt" v-model="db[config.id][input.key]"> {{opt}}
+                        </label>
+                    </div>
+
+                    <input v-else-if="input.key"
+                           :type="input.type === 'text' ? 'text' : 'number'"
+                           class="tax-input"
+                           :class="{'text-center': input.type === 'text' || input.align === 'center'}"
+                           v-model="db[config.id][input.key]"
+                           :readonly="input.isReadonly" />
+                </td>
+            </tr>
+        </tbody>
+    </table>
+    `
 }
